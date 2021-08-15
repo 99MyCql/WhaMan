8317b2ffcb2cd6dcae7b29f3897e8b00b94d7daa
dounine <949835972@qq.com>
2021-08-15 11:36:19 +0800

    feat(restock): 完善进货功能，ID重复后自增

--- app/restock/service/impl/restock.go
+++ app/restock/service/impl/restock.go
@@ -1,6 +1,8 @@
 package impl
 
 import (
+	"fmt"
+
 	restockModel "WhaMan/app/restock/model"
 	stockModel "WhaMan/app/stock/model"
 	supplierModel "WhaMan/app/supplier/model"
@@ -15,19 +17,35 @@ type Restock struct {
 
 // Restock 新增进货订单，并新增库存，以及更新供应商交易额信息
 func (r *Restock) Restock(i *restockModel.RestockInfo) error {
-	// TODO: ID重复
-	restockOrder := GenRestockOrder(i)
-	stock := GenStock(i)
+	// 生成进货订单ID=库存ID
+	var id string
+	for index := 1; ; index++ {
+		id = GenID(i, index)
+		if id == "" {
+			return errors.Errorf("进货流程中，ID超出编号范围：%s", id)
+		}
+		if err := global.DB.First(&restockModel.RestockOrder{}, "id = ?", id).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				break
+			} else {
+				return errors.Wrapf(err, "进货流程中，查询进货订单ID是否重复失败：%s", id)
+			}
+		}
+	}
+
+	// 执行事务：保证新增库存、新增进货订单、更新供应商信息等操作同时成功
+	restockOrder := GenRestockOrder(i, id)
+	stock := GenStock(i, id)
 	return global.DB.Transaction(func(tx *gorm.DB) error {
 		if err := tx.Create(stock).Error; err != nil {
-			return errors.Wrapf(err, "新增库存失败：%+v", stock)
+			return errors.Wrapf(err, "进货流程中，新增库存失败：%+v", stock)
 		}
 		restockOrder.StockID = stock.ID
 		if err := tx.Create(restockOrder).Error; err != nil {
-			return errors.Wrapf(err, "新增进货订单失败：%+v", restockOrder)
+			return errors.Wrapf(err, "进货流程中，新增进货订单失败：%+v", restockOrder)
 		}
 		var supplier supplierModel.Supplier
-		if err := tx.Where("name = ?", i.SupplierName).First(&supplier).Error; err != nil {
+		if err := tx.First(&supplier, "name = ?", i.SupplierName).Error; err != nil {
 			return errors.Wrapf(err, "进货流程中，获取供应商信息失败：%s", i.SupplierName)
 		}
 		supplier.Turnover += i.SumMoney
@@ -55,9 +73,9 @@ func (r *Restock) Delete(id string) error {
 }
 
 // GenRestockOrder 根据进货信息生成进货订单
-func GenRestockOrder(i *restockModel.RestockInfo) *restockModel.RestockOrder {
+func GenRestockOrder(i *restockModel.RestockInfo, id string) *restockModel.RestockOrder {
 	return &restockModel.RestockOrder{
-		ID:            GenID(i),
+		ID:            id,
 		Date:          i.Date,
 		Model:         i.Model,
 		Specification: i.Specification,
@@ -73,9 +91,9 @@ func GenRestockOrder(i *restockModel.RestockInfo) *restockModel.RestockOrder {
 }
 
 // GenStock 根据进货信息生成库存信息
-func GenStock(i *restockModel.RestockInfo) *stockModel.Stock {
+func GenStock(i *restockModel.RestockInfo, id string) *stockModel.Stock {
 	return &stockModel.Stock{
-		ID:              GenID(i),
+		ID:              id,
 		Model:           i.Model,
 		Specification:   i.Specification,
 		RestockQuantity: i.Quantity,
@@ -88,6 +106,9 @@ func GenStock(i *restockModel.RestockInfo) *stockModel.Stock {
 	}
 }
 
-func GenID(i *restockModel.RestockInfo) string {
-	return i.Date.Format("20060102") + "-" + i.SupplierName + "-" + i.Model + "-0001"
+func GenID(i *restockModel.RestockInfo, index int) string {
+	if index >= 10000 {
+		return ""
+	}
+	return i.Date.Format("20060102") + "-" + i.SupplierName + "-" + i.Model + fmt.Sprintf("-%04d", index)
 }
